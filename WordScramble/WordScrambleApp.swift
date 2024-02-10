d17dea42eafa805a22ac7bf187bd8f3443ed169d
Baymurat785 <abdimuratovbaymurat03@gmail.com>
2024-02-10 14:32:26 +0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Baymurat Abdumuratov on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
